3b2a7aa69b2a8de28db45f9aa62f4e3b3fd7fea0
Katie Stairs <kstairs831@gmail.com>
2023-06-07 12:23:47 -0500

    Did some more organization and styling.

--- src/components/StartStory/StartStory.jsx
+++ src/components/StartStory/StartStory.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import Input from 'bootstrap'
 import './StartStory.css';
@@ -6,23 +6,43 @@ import './StartStory.css';
 const StartStory = () => {
   const {
     transcript,
+    interimTranscript,
+    finalTranscript,
     listening,
     resetTranscript,
     browserSupportsSpeechRecognition
-  } = useSpeechRecognition();
+    } = useSpeechRecognition();
+const startListening = () => SpeechRecognition.startListening({ continuous: true });
 
-  if (!browserSupportsSpeechRecognition) {
-    return <span>Browser doesn't support speech recognition.</span>;
+useEffect(() => {
+  if (finalTranscript !== '') {
+    console.log('Got the final result:', finalTranscript);
   }
+}, [interimTranscript, finalTranscript]);
+
+
+if (!browserSupportsSpeechRecognition) {
+  return <span>Browser doesn't support speech recognition.</span>;
+}
 
   return (
-    <div className="card" style={{ width: 225 }}>
+    <div className="card" style={{ width: 500 }}>
       <div className="card-body">
-        <p>Microphone: {listening ? 'on' : 'off'}</p>
-        <button onClick={SpeechRecognition.startListening}>Start</button>
-        <button onClick={SpeechRecognition.stopListening}>Stop</button>
-        <button onClick={resetTranscript}>Reset</button>
+        <ul style={{ padding: 10 }}>
+          <li>Press Start to begin recording and the microphone will record continuously.</li>
+          <li>Press Stop and the recording will pause until it is reset.</li>
+          <li>Press Reset to clear the transcript.</li>
+          <li>Make sure to copy the text from the box into the correct input below!</li>
+        </ul>
+        <h5 style={{ padding: 10 }}>Microphone: {listening ? 'on' : 'off'}</h5>
+        <button style={{ margin: 3 }} onClick={SpeechRecognition.startListening}>Start</button>
+        <button style={{ margin: 3 }} onClick={SpeechRecognition.stopListening}>Stop</button>
+        <button style={{ margin: 3 }} onClick={resetTranscript}>Reset</button>
+      </div>
+      <div className="card" style={{ width: 500 }}>
+        <div className="card-body">
         <p>{transcript}</p>
+        </div>
       </div>
     </div>
   );
